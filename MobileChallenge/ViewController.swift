ed781c2cd2636aee79be8512087efc82da6f07c5
Juan Cardozo <jcar@MacBook-Pro-de-Juan.local>
2017-06-29 10:56:28 -0400

    Initial Commit

---
+++ MobileChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileChallenge
+//
+//  Created by Juan Carlos Cardozo on 29/6/17.
+//  Copyright © 2017 Juan Carlos Cardozo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
